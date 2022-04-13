3f100e3a3a603a38a64ef15ae1e41dfaf4cbc74a
Serhii Horoshko <sr.horoshko@gmail.com>
2022-04-13 20:26:11 +0300

    Added: createBook, getBook, updateBook, removeBook functions, change database structure and create validation

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-Windows, Develop: `npm run windowsDebug`
+- Required mongodb: `mongod` command
+- Windows, Develop: `npm run windowsDebug` command
+
+### Comparison Query Operators:
+- eq (equal),
+- ne (not equal),
+- gt (greather than)
+- gte (greathe than or equal to)
+- lt (less than)
+- lte (less than or equal to)
+- in
+- nin (not in)
+
+### Logical Query Operators
+ - or
+ - and
+
+### Regular Expression
+[$regex manual](https://www.mongodb.com/docs/manual/reference/operator/query/regex/)
+
+Examples `{ <field>: /pattern/<options> }`: 
+- Start with _Tolkien_ `.find ({ author: /^Tolkien/ })`
+- Ends with _Tolkien_ `.find ({ author: /Tolkien$/i })`
+- Contain _Tolkien_ `.find ({ author: /.*Tolkien.*/ })`
+
+### Counting
+
+`.count();`
--- index.js
+++ index.js
@@ -6,24 +6,75 @@ mongoose.connect('mongodb://localhost/bookstore')
     .catch(err => debug('Could not connect to MongoDB...'. err));
 
 const bookSchema = new mongoose.Schema({
-    name: String,
+    name: { 
+        type: String, 
+        required: true,
+        minlength: 4,
+        maxlength: 255 
+    },
+    category: { 
+        type: String,
+        required: true,
+        enum: ['Judgment', 'Survival','Peace and war', 'Love', 
+        'Heroism', 'Good and evil', 'Circle of life', 'Suffering', 
+        'Deception', 'Coming of age']
+     },
     author: String,
     tags: [ String ],
     date: { type: Date, default: Date.now },
-    isPublished: Boolean
+    isPublished: Boolean,
+    price: { 
+        type: Number,
+        required: function() { return this.isPublished; } 
+    }
 });
 
 const Book = mongoose.model('Book', bookSchema);
+
 async function createBook(){
     const book = new Book({
-        name: 'The Hobbit',
-        author: 'J.R.R. Tolkien’s',
+        name: 'The Fellowship of the Ring',
+        category: 'Peace and war',
+        author: 'J.R.R. Tolkien',
         tags: ['tolkien', 'lordoftherings'],
-        isPublished: true
+        isPublished: true,
+        price: 33
     });
     
+    try {
+        const result = await book.save();
+        debug(result);
+    } catch (error) {
+        debug(error.message);
+    }
+};
+
+async function getBooks() {
+    const books = await Book
+        .find({ author: 'J.R.R. Tolkien', isPublished: true, price: { $gte: 10, $lte: 20 } })
+        .limit(10)
+        .sort({ name: 1 })
+        .select({ name: 1, tags: 1 });
+    debug(books);
+};
+
+async function updateBook(id) {
+    const book = await Book.findById(id);
+    if (!book) return;
+
+    book.isPublished = true;
+    book.author = 'J.R.R. Tolkien';
+
     const result = await book.save();
     debug(result);
 };
 
+async function removeBook(id) {
+    const result = await Book.deleteOne({ _id: id });
+    debug(result);
+};
+
 createBook();
+getBooks();
+updateBook();
+removeBook();
