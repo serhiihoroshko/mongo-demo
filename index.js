3ce1b00de82105abd7fbd60219729f17e151b291
Serhii Horoshko <sr.horoshko@gmail.com>
2022-04-14 20:47:31 +0300

    Re-format .js document for read

--- index.js
+++ index.js
@@ -1,86 +1,101 @@
 const mongoose = require('mongoose');
 const debug = require('debug')('app:db');
 
-mongoose.connect('mongodb://localhost/bookstore')
-    .then(() => debug('Connected to MongoDB...'))
-    .catch(err => debug('Could not connect to MongoDB...'. err));
+mongoose
+  .connect('mongodb://localhost/bookstore')
+  .then(() => debug('Connected to MongoDB...'))
+  .catch((err) => debug('Could not connect to MongoDB...'.err));
 
 const bookSchema = new mongoose.Schema({
-    name: { 
-        type: String, 
-        required: true,
-        minlength: 4,
-        maxlength: 255 
+  name: {
+    type: String,
+    required: true,
+    minlength: 4,
+    maxlength: 255,
+  },
+  category: {
+    type: String,
+    required: true,
+    enum: [
+      'Judgment',
+      'Survival',
+      'Peace and war',
+      'Love',
+      'Heroism',
+      'Good and evil',
+      'Circle of life',
+      'Suffering',
+      'Deception',
+      'Coming of age',
+    ],
+  },
+  author: String,
+  tags: {
+    type: Array,
+    validate: {
+      validator: function (tagValue) {
+        return tagValue && tagValue.length > 0;
+      },
+      message: 'A book should have at least one tag.',
     },
-    category: { 
-        type: String,
-        required: true,
-        enum: ['Judgment', 'Survival','Peace and war', 'Love', 
-        'Heroism', 'Good and evil', 'Circle of life', 'Suffering', 
-        'Deception', 'Coming of age']
-     },
-    author: String,
-    tags: { 
-        type: Array,
-        validate: { 
-            validator: function(tagValue) {
-                return tagValue && tagValue.length > 0;
-            },
-            message: 'A book should have at least one tag.'
-         }
-     },
-    date: { type: Date, default: Date.now },
-    isPublished: Boolean,
-    price: { 
-        type: Number,
-        required: function() { return this.isPublished; } 
-    }
+  },
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean,
+  price: {
+    type: Number,
+    required: function () {
+      return this.isPublished;
+    },
+  },
 });
 
 const Book = mongoose.model('Book', bookSchema);
 
-async function createBook(){
-    const book = new Book({
-        name: 'Tolkien: Maker of Middle-earth',
-        category: 'Peace and war',
-        author: 'J.R.R. Tolkien',
-        tags: ['tolkien', 'lordoftherings'],
-        isPublished: true,
-        price: 49
-    });
-    
-    try {
-        const result = await book.save();
-        debug(result);
-    } catch (error) {
-        debug(error.message);
-    }
-};
+async function createBook() {
+  const book = new Book({
+    name: 'Tolkien: Maker of Middle-earth',
+    category: 'Peace and war',
+    author: 'J.R.R. Tolkien',
+    tags: ['tolkien', 'lordoftherings'],
+    isPublished: true,
+    price: 49,
+  });
 
-async function getBook() {
-    const books = await Book
-        .find({ author: 'J.R.R. Tolkien', isPublished: true, price: { $gte: 10, $lte: 20 } })
-        .limit(10)
-        .sort({ name: 1 })
-        .select({ name: 1, tags: 1 });
-    debug(books);
-};
-
-async function updateBook(id) {
-    const book = await Book.findById(id);
-    if (!book) return;
-
-    book.isPublished = true;
-    book.price = 16;
-
+  try {
     const result = await book.save();
     debug(result);
-};
+  } catch (error) {
+    debug(error.message);
+  }
+}
+
+async function getBook() {
+  const books = await Book.find({
+    author: 'J.R.R. Tolkien',
+    isPublished: true,
+    price: { $gte: 10, $lte: 20 },
+  })
+    .limit(10)
+    .sort({ name: 1 })
+    .select({ name: 1, tags: 1 });
+  debug(books);
+}
+
+async function updateBook(id) {
+  const book = await Book.findById(id);
+  if (!book) return;
+
+  book.isPublished = true;
+  book.price = 16;
+
+  const result = await book.save();
+  debug(result);
+}
 
 async function removeBook(id) {
-    const result = await Book.deleteOne({ _id: id });
-    debug(result);
-};
+  const result = await Book.deleteOne({ _id: id });
+  debug(result);
+}
 
 createBook();
 getBook();
